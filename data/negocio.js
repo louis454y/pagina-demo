c5d541b432a1e498b8b60049dcf512974e65b17f
louis454y <butronluis02@gmail.com>
2026-08-21 07:22:42 -0500

    dd

--- data/negocio.js
+++ data/negocio.js
@@ -247,7 +247,7 @@ nosotros: {
         whatsapp: "573216530020",
 
         mensajeWhatsapp:
-            "Hola Beia Beauty Space, quiero información sobre sus servicios.",
+            "Hola Beia Beauty Space, quiero información sobre sus servicios por favor.",
 
         instagram:
             "https://www.instagram.com/beiabeauty_/",

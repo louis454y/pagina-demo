7c011a27e30754bcb6c8a8d635f764c4440ac8cc
louis454y <butronluis02@gmail.com>
2026-08-21 07:16:54 -0500

    c

--- data/negocio.js
+++ data/negocio.js
@@ -247,7 +247,7 @@ nosotros: {
         whatsapp: "573012261366",
 
         mensajeWhatsapp:
-            "Hola Beauty Caprice, quiero información sobre sus servicios por favor.",
+            "Holaa Beauty Caprice, quiero información sobre sus servicios por favor.",
 
         instagram:
             "https://www.instagram.com/beautycapricee/",
